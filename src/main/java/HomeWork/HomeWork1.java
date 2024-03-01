9ae8da2a49ef81eeff4ac23b97d9735125b7716e
stani4 <37027670+stani4@users.noreply.github.com>
2024-03-01 18:48:25 +0200

    Initial commit

---
+++ src/main/java/HomeWork/HomeWork1.java
@@ -0,0 +1,18 @@
+package HomeWork;
+
+import java.util.Scanner;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please, enter your name:");
+                String name = scanner.nextLine();
+
+        System.out.println("Please, enter your hoby");
+                String hoby = scanner.nextLine();
+
+        System.out.println("Nice to meet you "+ name +" your hoby "+ hoby +" is very interesting");
+    }
+}
