17b29982913895ef6b0bb76c95eeb860172ae361
stani4 <37027670+stani4@users.noreply.github.com>
2024-03-10 15:04:20 +0200

    HomeWork2d is ready

---
+++ src/main/java/HomeWorks/HomeWork2d.java
@@ -0,0 +1,14 @@
+package HomeWorks;
+
+public class HomeWork2d {
+
+    public static void main(String[] args) {
+         String str1 = "word1";
+
+         String str2 = "word2";
+
+         String result = str1.concat(str2);
+
+        System.out.println(result);
+    }
+}
